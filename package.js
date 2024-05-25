ff7a4944acbdc95d6e9947e98c45c7b506f943b9
Pierre Wieser <p.wieser@trychlos.org>
2024-05-25 16:57:31 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:editor',
-    version: '1.4.2',
+    version: '1.4.3-rc',
     summary: 'An encapsulation of "trumbowyg" for Meteor',
     git: 'https://github.com/trychlos/pwix-editor',
     documentation: 'README.md'
